cfc6730b53a1c02de78bb0623311ccdae142dbf9
azzahra <azzahra@Azzahra>
2023-05-20 21:21:00 +0700

    pertemuan7

---
+++ pert7.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+    float a, b, c;
+
+    cout<<"angka 1 : ";
+    cin>>a;
+
+    cout<<"angka 2 : ";
+    cin>>b;
+
+    c = a + b;
+    cout<<c<<endl;
+
+}
